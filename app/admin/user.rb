f763b6fe2f698ffd231bb45bb0a70bac399ca41d
bharatupgraded <bharatupgraded@gmail.com>
2014-12-30 12:35:58 +0530

    app/admin/user.rb

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -33,10 +33,27 @@ ActiveAdmin.register User do
             f.input :email
             f.input :password
             f.input :password_confirmation
-            f.input :role, as: :radio, collection: {Author: "author", Moderator: "moderator", Administrator: "administrator"}
+            f.input :role, as: :radio, collection: {Author: "author", SuperAdmin: "super_admin", Administrator: "administrator"}
             f.input :approved
         end
         f.actions
     end
+
+    controller do
+        def update
+          p "111111111111111111111"
+                  params["user"]
+          p "222222222222222222"
+          user = params["user"]
+            # If we haven't set a password explicitly, we don't want it reset so 
+            # don't pass those fields upstream and devise will ignore them
+          if  user && (user["password"] == nil || user["password"].empty?)
+              user.delete("password")
+              user.delete("password_confirmation")
+        end
+
+        update!
+    end
+end
 
 end
